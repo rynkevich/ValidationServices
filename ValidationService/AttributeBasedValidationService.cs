aa83334e4339af96a4727188070e051024726360
NRGb3nder <energybender120@gmail.com>
2018-09-08 06:37:34 +0300

    Fix incorrect validation of null

--- ValidationService/AttributeBasedValidationService.cs
+++ ValidationService/AttributeBasedValidationService.cs
@@ -12,6 +12,10 @@ namespace ValidationService {
         }
 
         public override GeneralConclusion Validate(object obj, string objName = "") {
+            if (obj == null) {
+                return new GeneralConclusion(isValid: true);
+            }
+
             if (this.IsRecursiveValidation) {
                 this.hashCodeSet = new HashSet<int>();
                 this.hashCodeSet.Add(obj.GetHashCode());
@@ -24,26 +28,23 @@ namespace ValidationService {
         }
 
         private GeneralConclusion ValidateObject(object obj, string fullName) {
-            if (obj == null) {
-                return new GeneralConclusion(isValid: true);
-            }
-
             GeneralConclusion conclusion = new GeneralConclusion(isValid: true);
+
             foreach (PropertyInfo prop in obj.GetType().GetProperties()) {
-                object value = prop.GetValue(obj);
-                if (!this.IsRecursiveValidation || value == null || !this.hashCodeSet.Contains(value.GetHashCode())) {
-                    if (value != null) {
-                        this.hashCodeSet.Add(value.GetHashCode());
-                    }
-                    if (this.IsRecursiveValidation) {
-                        conclusion += this.ValidateObject(value, fullName + '.' + prop.Name);
-                    }
-                    foreach (Attribute attr in prop.GetCustomAttributes()) {
-                        if (attr is IValidator) {
-                            ElementaryConclusion elemConclusion = (attr as IValidator).Validate(value);
-                            conclusion += new ElementaryConclusion(elemConclusion.IsValid,
-                                elemConclusion.Details != null ? (fullName +
-                                '.' + prop.Name + ": " + elemConclusion.Details) : null);
+                if (prop.GetIndexParameters().Length == 0) {
+                    object value = prop.GetValue(obj);
+                    if (!this.IsRecursiveValidation || value == null || !this.hashCodeSet.Contains(value.GetHashCode())) {
+                        if (this.IsRecursiveValidation && value != null) {
+                            this.hashCodeSet.Add(value.GetHashCode());
+                            conclusion += this.ValidateObject(value, fullName + '.' + prop.Name);
+                        }
+                        foreach (Attribute attr in prop.GetCustomAttributes()) {
+                            if (attr is IValidator) {
+                                ElementaryConclusion elemConclusion = (attr as IValidator).Validate(value);
+                                conclusion += new ElementaryConclusion(elemConclusion.IsValid,
+                                    elemConclusion.Details != null ? (fullName +
+                                    '.' + prop.Name + ": " + elemConclusion.Details) : null);
+                            }
                         }
                     }
                 }

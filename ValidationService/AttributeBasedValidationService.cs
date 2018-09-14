cc8a1cec50f5d67bf01862326290cdd6436fc3fd
NRGb3nder <energybender120@gmail.com>
2018-09-15 00:39:34 +0300

    Fix recursive validation bug

--- ValidationService/AttributeBasedValidationService.cs
+++ ValidationService/AttributeBasedValidationService.cs
@@ -8,10 +8,11 @@ namespace ValidationService
 {
     public class AttributeBasedValidationService : ValidationService
     {
-        private HashSet<int> hashCodeSet;
+        private readonly Stack<object> trace;
 
         public AttributeBasedValidationService(bool isRecursiveValidation = true)
         {
+            this.trace = new Stack<object>();
             this.IsRecursiveValidation = isRecursiveValidation;
         }
 
@@ -22,51 +23,47 @@ namespace ValidationService
                 return new GeneralConclusion(isValid: true);
             }
 
-            if (this.IsRecursiveValidation)
-            {
-                this.hashCodeSet = new HashSet<int>
-                {
-                    obj.GetHashCode()
-                };
-                GeneralConclusion conclusion = this.ValidateObject(obj, objName);
-                this.hashCodeSet = null;
-                return conclusion;
-            }
-            else
-            {
-                return this.ValidateObject(obj, objName);
-            }
+            return this.ValidateObject(obj, objName);
         }
 
-        private GeneralConclusion ValidateObject(object obj, string fullName)
+        private GeneralConclusion ValidateObject<T>(T obj, string fullName) where T : class
         {
-            GeneralConclusion conclusion = new GeneralConclusion(isValid: true);
+            if (this.IsRecursiveValidation)
+            {
+                this.trace.Push(obj);
+            }
 
+            GeneralConclusion conclusion = new GeneralConclusion(isValid: true);
+            if (obj == null)
+            {
+                return conclusion;
+            }
             foreach (PropertyInfo prop in obj.GetType().GetProperties())
             {
                 if (prop.GetIndexParameters().Length == 0)
                 {
                     object value = prop.GetValue(obj);
-                    if (!this.IsRecursiveValidation || value == null || !this.hashCodeSet.Contains(value.GetHashCode()))
+
+                    if (this.IsRecursiveValidation && !this.trace.Contains(value))
                     {
-                        if (this.IsRecursiveValidation && value != null)
-                        {
-                            this.hashCodeSet.Add(value.GetHashCode());
-                            conclusion += this.ValidateObject(value, $"{fullName}.{prop.Name}");
-                        }
-                        foreach (ValidationAttribute attr in prop.GetCustomAttributes<ValidationAttribute>())
-                        {
-                            ElementaryConclusion elemConclusion = attr.Validate(value);
-                            conclusion += new ElementaryConclusion(elemConclusion.IsValid,
-                                elemConclusion.Details != null ?
-                                $"{fullName}.{prop.Name}: {elemConclusion.Details}" : null);
-                        }
+                        conclusion += this.ValidateObject(value, $"{fullName}.{prop.Name}");
+                    }
+
+                    foreach (ValidationAttribute attr in prop.GetCustomAttributes<ValidationAttribute>())
+                    {
+                        ElementaryConclusion elemConclusion = attr.Validate(value);
+                        conclusion += new ElementaryConclusion(elemConclusion.IsValid,
+                            elemConclusion.Details != null ?
+                            $"{fullName}.{prop.Name}: {elemConclusion.Details}" : null);
                     }
                 }
             }
 
+            if (this.IsRecursiveValidation)
+            {
+                this.trace.Pop();
+            }
             return conclusion;
         }
-
     }
 }

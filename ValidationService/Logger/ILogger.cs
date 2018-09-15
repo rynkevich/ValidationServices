96fb8e8190c02f5ad21ba74d6532bfbcbda6831d
NRGb3nder <energybender120@gmail.com>
2018-09-15 06:35:13 +0300

    Add ILoger interface and NLogLogger proxy class

---
+++ ValidationService/Logger/ILogger.cs
@@ -0,0 +1,38 @@
+﻿namespace ValidationService.Logger
+{
+    /// <summary>
+    /// Logger interface.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Log using "Debug" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        void Debug(string message);
+
+        /// <summary>
+        /// Log using "Info" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        void Info(string message);
+
+        /// <summary>
+        /// Log using "Warn" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        void Warn(string message);
+
+        /// <summary>
+        /// Log using "Error" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        void Error(string message);
+
+        /// <summary>
+        /// Log using "Fatal" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        void Fatal(string message);
+    }
+}
---
+++ ValidationService/Logger/NLogLogger.cs
@@ -0,0 +1,64 @@
+﻿namespace ValidationService.Logger
+{
+    /// <summary>
+    /// NLog Proxy.
+    /// </summary>
+    public class NLogLogger : ILogger
+    {
+        /// <summary>
+        /// Actual logger.
+        /// </summary>
+        private readonly NLog.Logger logger;
+
+        /// <param name="utility">The actual logger</param>
+        public NLogLogger(NLog.Logger utility)
+        {
+            this.logger = utility;
+        }
+
+        /// <summary>
+        /// Log using "Debug" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        public void Debug(string message)
+        {
+            this.logger.Debug(message);
+        }
+
+        /// <summary>
+        /// Log using "Info" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        public void Info(string message)
+        {
+            this.logger.Info(message);
+        }
+
+        /// <summary>
+        /// Log using "Warn" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        public void Warn(string message)
+        {
+            this.logger.Warn(message);
+        }
+
+        /// <summary>
+        /// Log using "Error" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        public void Error(string message)
+        {
+            this.logger.Error(message);
+        }
+
+        /// <summary>
+        /// Log using "Fatal" log level.
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        public void Fatal(string message)
+        {
+            this.logger.Fatal(message);
+        }
+    }
+}

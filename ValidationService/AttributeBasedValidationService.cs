c2a1a3a861fdb30109e351735df3e4ddece02165
NRGb3nder <energybender120@gmail.com>
2018-09-15 04:28:33 +0300

    Provide documentation on service classes and IValidator

--- ValidationService/AttributeBasedValidationService.cs
+++ ValidationService/AttributeBasedValidationService.cs
@@ -6,16 +6,39 @@ using ValidationService.Attributes;
 
 namespace ValidationService
 {
+    /// <summary>
+    /// Attribute based service of object validation.
+    /// </summary>
     public class AttributeBasedValidationService : ValidationService
     {
+        /// <summary>
+        /// Stack of objects that are validating at the moment.
+        /// Is maintained to avoid loops.
+        /// </summary>
         private readonly Stack<object> trace;
 
+        /// <summary>
+        /// Service constructor.
+        /// </summary>
+        /// <param name="isRecursiveValidation">The flag indicating whether the validation should be accomplished recursively.</param>
         public AttributeBasedValidationService(bool isRecursiveValidation = true)
         {
             this.trace = new Stack<object>();
             this.IsRecursiveValidation = isRecursiveValidation;
         }
 
+        /// <summary>
+        /// Override of <see cref="ValidationService.Validate{T}(T, string))"/>
+        /// </summary>
+        /// <typeparam name="T">The type of object to be validated</typeparam>
+        /// <param name="obj">The object to validate</param>
+        /// <param name="objName">The object name. 
+        /// Used to print full qualified property names to <see cref="GeneralConclusion.Details"/></param>
+        /// <returns>
+        /// <see cref="GeneralConclusion"/> with <c>IsValid</c> flag set to <c>true</c> 
+        /// if the <paramref name="obj"/> is acceptable. Otherwise, the flag is set to <c>false</c> and
+        /// <see cref="GeneralConclusion.Details"/> contains a report on problems.
+        /// </returns>
         public override GeneralConclusion Validate<T>(T obj, string objName = "")
         {
             if (obj == null)
@@ -26,6 +49,18 @@ namespace ValidationService
             return this.ValidateObject(obj, objName);
         }
 
+        /// <summary>
+        /// Internal logic of object validation.
+        /// Is called recursively if <see cref="ValidationService.isRecursiveValidation"/> equals <c>true</c>
+        /// </summary>
+        /// <typeparam name="T">The type of object to be validated</typeparam>
+        /// <param name="obj">The object to validate</param>
+        /// <param name="fullName">The full object name</param>
+        /// <returns>
+        /// <see cref="GeneralConclusion"/> with <c>IsValid</c> flag set to <c>true</c> 
+        /// if the <paramref name="obj"/> is acceptable. Otherwise, the flag is set to <c>false</c> and
+        /// <see cref="GeneralConclusion.Details"/> contains a report on problems.
+        /// </returns>
         private GeneralConclusion ValidateObject<T>(T obj, string fullName) where T : class
         {
             if (this.IsRecursiveValidation)

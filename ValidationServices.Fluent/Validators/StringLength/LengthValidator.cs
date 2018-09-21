ef1fab0111a2b17a7454baebd49d319128ef256b
NRGb3nder <energybender120@gmail.com>
2018-09-21 04:18:15 +0300

    Add string length validators

---
+++ ValidationServices.Fluent/Validators/StringLength/LengthValidator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ValidationServices.Results;
+
+namespace ValidationServices.Fluent.Validators.StringLength
+{
+    public class LengthValidator : IPropertyValidator
+    {
+        private readonly int min;
+        private readonly int max;
+        private readonly Func<object, int> minFunc;
+        private readonly Func<object, int> maxFunc;
+        protected static readonly int MAX_NOT_SPECIFIED = -1;
+
+        public string FailureMessage { get; set; } = "Length of a string must satisfy specified constraints";
+
+        public LengthValidator(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+
+            if (max != MAX_NOT_SPECIFIED && min > max)
+            {
+                throw new ArgumentOutOfRangeException(nameof(max), "Max should be larger than min");
+            }
+        }
+
+        public LengthValidator(Func<object, int> minFunc, Func<object, int> maxFunc)
+        {
+            this.minFunc = minFunc ?? throw new ArgumentNullException(nameof(minFunc));
+            this.maxFunc = maxFunc ?? throw new ArgumentNullException(nameof(maxFunc));
+        }
+
+        public ElementaryConclusion Validate(object objectToValidate)
+        {
+            if (objectToValidate == null)
+            {
+                return new ElementaryConclusion(isValid: false, this.FailureMessage);
+            }
+
+            int min = this.minFunc != null ? this.minFunc(objectToValidate) : this.min;
+            int max = this.maxFunc != null ? this.maxFunc(objectToValidate) : this.max;
+
+            int length = objectToValidate.ToString().Length;
+
+            if (length < min || (length > max && max != MAX_NOT_SPECIFIED))
+            {
+                return new ElementaryConclusion(isValid: false, this.FailureMessage);
+            }
+
+            return new ElementaryConclusion(isValid: true);
+        }
+    }
+}
---
+++ ValidationServices.Fluent/Validators/StringLength/MaxLengthValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ValidationServices.Fluent.Validators.StringLength
+{
+    public class MaxLengthValidator : LengthValidator
+    {
+        public MaxLengthValidator(int max) : base(0, max)
+        {
+        }
+
+        public MaxLengthValidator(Func<object, int> max) : base(obj => 0, max)
+        {
+        }
+    }
+}

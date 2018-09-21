a9079d943e0da6fe398db6b12b913f10a9981dbd
NRGb3nder <energybender120@gmail.com>
2018-09-21 04:14:17 +0300

    Add validation service skeleton

---
+++ ValidationServices.Fluent/Service/CustomValidationService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using ValidationServices.Service;
+using ValidationServices.Results;
+using ValidationServices.Service.Exceptions;
+using ValidationServices.Fluent.Rules;
+
+namespace ValidationServices.Fluent.Service
+{
+    using TypeValidationRule = Dictionary<string, IPropertyValidationRule>;
+
+    public class CustomValidationService : BaseValidationService
+    {
+        private readonly Stack<object> trace;
+        private readonly Dictionary<Type, TypeValidationRule> typesToValidate;
+
+        public override bool IsRecursiveValidation { get; set; }
+
+        public CustomValidationService(bool isRecursiveValidation = true)
+        {
+            this.IsRecursiveValidation = isRecursiveValidation;
+            this.trace = new Stack<object>();
+            this.typesToValidate = new Dictionary<Type, TypeValidationRule>();
+        }
+
+        public override GeneralConclusion Validate<T>(T objectToValidate, string objName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRule(Type validationTarget, 
+            string propertyName, IPropertyValidationRule newPropertyValidationRule)
+        {
+            if (validationTarget == null)
+            {
+                throw new ArgumentNullException(nameof(validationTarget));
+            }
+            if (propertyName == null)
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+            if (newPropertyValidationRule == null)
+            {
+                throw new ArgumentNullException(nameof(newPropertyValidationRule));
+            }
+
+            if (this.typesToValidate.TryGetValue(validationTarget, out TypeValidationRule typeValidationRule))
+            {
+                if (typeValidationRule.ContainsKey(propertyName))
+                {
+                    throw new InvalidOperationException($"Validation rule for property '{propertyName}' is already set");
+                }
+                typeValidationRule.Add(propertyName, newPropertyValidationRule);
+            }
+            else
+            {
+                TypeValidationRule newTypeValidationRule = new TypeValidationRule();
+                newTypeValidationRule.Add(propertyName, newPropertyValidationRule);
+                this.typesToValidate.Add(validationTarget, newTypeValidationRule);
+            }
+        }
+
+        private GeneralConclusion ValidateObject<T>(T objectToValidate, string fullName)
+        {
+            if (this.IsRecursiveValidation)
+            {
+                this.trace.Push(objectToValidate);
+            }
+
+            GeneralConclusion conclusion = new GeneralConclusion(isValid: true);
+            TypeValidationRule typeValidationRule;
+            if (objectToValidate == null || !this.typesToValidate.TryGetValue(typeof(T), out typeValidationRule))
+            {
+                return conclusion;
+            }
+
+            foreach (PropertyInfo property in objectToValidate.GetType().GetProperties())
+            {
+                if (typeValidationRule.TryGetValue(property.Name, out IPropertyValidationRule rule))
+                {
+                    conclusion += rule.Validate(property.GetValue(objectToValidate));
+                }
+            }
+
+            if (this.IsRecursiveValidation)
+            {
+                this.trace.Pop();
+            }
+            return conclusion;
+        }
+    }
+}
---
+++ ValidationServices.Fluent/Service/ValidationServiceBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using ValidationServices.Fluent.Rules;
+
+namespace ValidationServices.Fluent.Service
+{
+    public class ValidationServiceBuilder
+    {
+        private readonly CustomValidationService service;
+
+        public ValidationServiceBuilder(bool isRecursiveValidationService = true)
+        {
+            this.service = new CustomValidationService(isRecursiveValidationService);
+        }
+
+        public PropertyValidationRule<TOwner, TProperty> RuleFor<TOwner, TProperty>(
+            Expression<Func<TOwner, TProperty>> propertyLambda) where TOwner : class
+        {
+            MemberExpression memberExpression = propertyLambda.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                throw new ArgumentException($"Expression '{propertyLambda.ToString()}' does not refer to a property");
+            }
+            PropertyInfo propertyInfo = memberExpression.Member as PropertyInfo;
+            if (propertyInfo == null)
+            {
+                throw new ArgumentException($"Expression '{propertyLambda.ToString()}' refers to a field, not a property");
+            }
+            if (memberExpression.Expression != null)
+            {
+                throw new ArgumentException($"Expression '{propertyLambda.ToString()}' does not refer to a property of an argument");
+            }
+
+            PropertyValidationRule<TOwner, TProperty> validationRule = new PropertyValidationRule<TOwner, TProperty>();
+            this.service.SetRule(typeof(TOwner), propertyInfo.Name, validationRule);
+
+            return validationRule;
+        }
+
+        public CustomValidationService Build()
+        {
+            return this.service;
+        }
+    }
+}

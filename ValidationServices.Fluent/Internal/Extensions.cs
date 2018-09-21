9c2c499846919a3f7e517e86beb913284c2e3b2f
NRGb3nder <energybender120@gmail.com>
2018-09-21 04:17:20 +0300

    Add extension methods for internal use

---
+++ ValidationServices.Fluent/Internal/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidationServices.Fluent.Internal
+{
+    public static class Extensions
+    {
+        public static Func<object, int> CoerceToNonGeneric<T>(this Func<T, int> function)
+        {
+            return x => function((T)x);
+        }
+    }
+}

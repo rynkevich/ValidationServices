fad54c2f737f4dc729aff97f1ac6cafe012025cb
NRGb3nder <energybender120@gmail.com>
2018-09-21 04:16:42 +0300

    Add property validation rules interface and class

---
+++ ValidationServices.Fluent/Rules/IPropertyValidationRule.cs
@@ -0,0 +1,12 @@
+﻿using ValidationServices.Results;
+using ValidationServices.Fluent.Validators;
+
+namespace ValidationServices.Fluent.Rules
+{
+    public interface IPropertyValidationRule
+    {
+        GeneralConclusion Validate(object objectToValidate);
+        void SetPropertyValidator(IPropertyValidator propertyValidator);
+        void SetLastValidatorFailureMessage(string message);
+    }
+}
---
+++ ValidationServices.Fluent/Rules/PropertyValidationRule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using ValidationServices.Fluent.Validators;
+using ValidationServices.Results;
+
+namespace ValidationServices.Fluent.Rules
+{
+    public class PropertyValidationRule<TOwner, TProperty> : IPropertyValidationRule
+    {
+        private readonly List<IPropertyValidator> validators;
+
+        public PropertyValidationRule()
+        {
+            this.validators = new List<IPropertyValidator>();
+        }
+
+        public GeneralConclusion Validate(object objectToValidate)
+        {
+            GeneralConclusion conclusion = new GeneralConclusion(isValid: true);
+            foreach (IPropertyValidationRule validator in this.validators) {
+                conclusion += validator.Validate(objectToValidate);
+            }
+
+            return conclusion;
+        }
+
+        public void SetPropertyValidator(IPropertyValidator propertyValidator)
+        {
+            if (this.validators.Exists(validator => validator.GetType() == propertyValidator.GetType()))
+            {
+                throw new InvalidOperationException($"Validator of type '{propertyValidator.GetType()}' is already set in specified rule");
+            }
+            this.validators.Add(propertyValidator);
+        }
+
+        public void SetLastValidatorFailureMessage(string message)
+        {
+            if (this.validators.Count == 0)
+            {
+                throw new InvalidOperationException("There is no validator in rule to set failure message");
+            }
+
+            this.validators[this.validators.Count - 1].FailureMessage = message;
+        }
+    }
+}

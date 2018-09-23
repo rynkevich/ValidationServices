0607ff448f2b32d165ab7382beec403fbb25c709
NRGb3nder <energybender120@gmail.com>
2018-09-23 04:08:59 +0300

    Add tests for validation service with fluent syntax

---
+++ ValidationServices.Fluent.UnitTests/Service/ValidationServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+using System;
+using ValidationServices.Fluent.Service;
+using ValidationServices.Fluent.UnitTests.TestEntities;
+using ValidationServices.Fluent.Validators.Length;
+using ValidationServices.Fluent.Validators.Comparison;
+using ValidationServices.Fluent.Validators.Absence;
+
+namespace ValidationServices.Fluent.UnitTests.Service
+{
+    public class ValidationServiceTest
+    {
+        private readonly ServiceTestEntity testEntity;
+
+        public ValidationServiceTest()
+        {
+            this.testEntity = new ServiceTestEntity();
+        }
+
+        [Fact]
+        public void OnNullObjectToValidateThrowsArgumentNullExceptionTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CustomValidationService().Validate<object>(null));
+        }
+
+        [Fact]
+        public void OnRepeatingRuleForCallForSamePropertyThrowsInvalidOperationExceptionTest()
+        {
+            ValidationServiceBuilder serviceBuilder = new ValidationServiceBuilder();
+            serviceBuilder.RuleFor((ServiceTestEntity entity) => entity.EightCharString).MaxLength(10);
+            Assert.Throws<InvalidOperationException>(() => serviceBuilder.RuleFor(
+                (ServiceTestEntity entity) => entity.EightCharString).MinLength(5));
+        }
+
+        [Fact]
+        public void OnTryValidateNullExpressionThrowsArgumentExceptionTest()
+        {
+            ValidationServiceBuilder serviceBuilder = new ValidationServiceBuilder();
+            Assert.Throws<ArgumentException>(
+                () => serviceBuilder.RuleFor<ServiceTestEntity, object>(entity => null).Null());
+        }
+
+        [Fact]
+        public void OnTryValidateFieldThrowsArgumentExceptionTest()
+        {
+            ValidationServiceBuilder serviceBuilder = new ValidationServiceBuilder();
+            Assert.Throws<ArgumentException>(() => serviceBuilder.RuleFor(
+                (ServiceTestEntity entity) => entity.StringField).NotNull());
+        }
+
+        [Fact]
+        public void OnTryValidateMethodResultThrowsArgumentExceptionTest()
+        {
+            ValidationServiceBuilder serviceBuilder = new ValidationServiceBuilder();
+            Assert.Throws<ArgumentException>(() => serviceBuilder.RuleFor(
+                (ServiceTestEntity entity) => entity.GetFive()).Equal(5));
+        }
+    }
+}
---
+++ ValidationServices.Fluent.UnitTests/TestEntities/ServiceTestEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ValidationServices.Fluent.UnitTests.TestEntities
+{
+    public class ServiceTestEntity
+    {
+        public string StringField = "A string";
+
+        public string WhitespaceString { get; } = "  ";
+        public string NullString { get; } = null;
+        public string EightCharString { get; } = "A string";
+        public string NineCharString { get; } = "123456789";
+
+        public int Seven { get; } = 7;
+        public int Eight { get; } = 8;
+        public int Nine { get; } = 9;
+        public int Ten { get; } = 10;
+
+        public int GetFive()
+        {
+            return 5;
+        }
+    }
+}

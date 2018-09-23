7ce5e3d26e4e872624e4f07c8b749093dc9e4fb5
NRGb3nder <energybender120@gmail.com>
2018-09-24 00:20:45 +0300

    Provide documentation for internal extension methods

--- ValidationServices.Fluent/Internal/Extensions.cs
+++ ValidationServices.Fluent/Internal/Extensions.cs
@@ -2,8 +2,26 @@
 
 namespace ValidationServices.Fluent.Internal
 {
+    /// <summary>
+    /// Extension methods for internal use.
+    /// </summary>
     public static class Extensions
-    {
+    { 
+        /// <summary>
+        /// Throws <see cref="ArgumentNullException"/> if obj is null.
+        /// </summary>
+        /// <param name="obj">The object to check</param>
+        /// <param name="paramName">The object name to include in exception</param>
+        /// <param name="message">The message to include in exception</param>
+        internal static void Guard(this object obj, string paramName, string message = null)
+        {
+            if (obj == null)
+            {
+                throw (message == null) ? new ArgumentNullException(paramName) : new ArgumentNullException(paramName, message);
+            }
+        }
+
+#pragma warning disable 1591
         public static Func<object, int> CoerceToNonGeneric<T>(this Func<T, int> function)
         {
             return x => function((T)x);
@@ -13,13 +31,6 @@ namespace ValidationServices.Fluent.Internal
         {
             return x => function((T)x);
         }
-
-        internal static void Guard(this object obj, string paramName, string message = null)
-        {
-            if (obj == null)
-            {
-                throw (message == null) ? new ArgumentNullException(paramName) : new ArgumentNullException(paramName, message);
-            }
-        }
+#pragma warning restore 1591
     }
 }

8b66e3ae79d8f7cbca03e54b4933c9fd8295994a
NRGb3nder <energybender120@gmail.com>
2018-09-24 00:21:08 +0300

    Provide documentation for rules

--- ValidationServices.Fluent/Rules/IPropertyValidationRule.cs
+++ ValidationServices.Fluent/Rules/IPropertyValidationRule.cs
@@ -3,10 +3,28 @@ using ValidationServices.Fluent.Validators;
 
 namespace ValidationServices.Fluent.Rules
 {
+    /// <summary>
+    /// Interface that is used to generalize <see cref="IPropertyValidationRule"/>
+    /// </summary>
     public interface IPropertyValidationRule
     {
+        /// <summary>
+        /// Validates context.
+        /// </summary>
+        /// <param name="context">The context that includes information about object to validate</param>
+        /// <returns><see cref="ServiceConclusion"/> with validation result</returns>
         ServiceConclusion Validate(PropertyValidatorContext context);
+
+        /// <summary>
+        /// Sets property validator.
+        /// </summary>
+        /// <param name="propertyValidator">The validator to set</param>
         void SetPropertyValidator(IPropertyValidator propertyValidator);
+
+        /// <summary>
+        /// Sets custom failure message for last validator in rule.
+        /// </summary>
+        /// <param name="message">The failure message for validator</param>
         void SetLastValidatorFailureMessage(string message);
     }
 }
--- ValidationServices.Fluent/Rules/PropertyValidationRule.cs
+++ ValidationServices.Fluent/Rules/PropertyValidationRule.cs
@@ -5,15 +5,31 @@ using ValidationServices.Results;
 
 namespace ValidationServices.Fluent.Rules
 {
+    /// <summary>
+    /// Class that accumulates validators for single property.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the object that owns a property.</typeparam>
+    /// <typeparam name="TProperty">The type of the property to validate.</typeparam>
     public class PropertyValidationRule<TOwner, TProperty> : IPropertyValidationRule
     {
+        /// <summary>
+        /// Accumulator for validators.
+        /// </summary>
         private readonly List<IPropertyValidator> _validators;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertyValidationRule{TOwner, TProperty}"/> class.
+        /// </summary>
         public PropertyValidationRule()
         {
             this._validators = new List<IPropertyValidator>();
         }
 
+        /// <summary>
+        /// Validates context.
+        /// </summary>
+        /// <param name="context">The context that includes information about object to validate</param>
+        /// <returns><see cref="ServiceConclusion"/> with validation result</returns>
         public ServiceConclusion Validate(PropertyValidatorContext context)
         {
             var conclusion = new ServiceConclusion(isValid: true);
@@ -24,11 +40,20 @@ namespace ValidationServices.Fluent.Rules
             return conclusion;
         }
 
+        /// <summary>
+        /// Sets property validator.
+        /// </summary>
+        /// <param name="propertyValidator">The validator to set</param>
         public void SetPropertyValidator(IPropertyValidator propertyValidator)
         {
             this._validators.Add(propertyValidator);
         }
 
+        /// <summary>
+        /// Sets custom failure message for last validator in rule.
+        /// </summary>
+        /// <param name="message">The failure message for validator</param>
+        /// <exception cref="InvalidOperationException">Thrown if there is no validators in rule</exception>
         public void SetLastValidatorFailureMessage(string message)
         {
             if (this._validators.Count == 0)

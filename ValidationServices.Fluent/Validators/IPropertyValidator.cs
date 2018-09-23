318266eccbfdb0016d3ef3cde0ca20b831c68dfb
NRGb3nder <energybender120@gmail.com>
2018-09-24 00:22:04 +0300

    Provide documentation for IPropertyValidator and PropertyValidatorContext

--- ValidationServices.Fluent/Validators/IPropertyValidator.cs
+++ ValidationServices.Fluent/Validators/IPropertyValidator.cs
@@ -2,9 +2,21 @@
 
 namespace ValidationServices.Fluent.Validators
 {
+    /// <summary>
+    /// Contract for any property validator.
+    /// </summary>
     public interface IPropertyValidator
     {
+        /// <summary>
+        /// Gets or sets message to be returned in <see cref="ElementaryConclusion.Details"/> if validation fails.
+        /// </summary>
         string FailureMessage { get; set; }
+
+        /// <summary>
+        /// Validates specified context.
+        /// </summary>
+        /// <param name="context">The object with info that is required for validation</param>
+        /// <returns><see cref="ElementaryConclusion"/> with validation results</returns>
         ElementaryConclusion Validate(PropertyValidatorContext context);
     }
 }
--- ValidationServices.Fluent/Validators/PropertyValidatorContext.cs
+++ ValidationServices.Fluent/Validators/PropertyValidatorContext.cs
@@ -2,11 +2,26 @@
 
 namespace ValidationServices.Fluent.Validators
 {
+    /// <summary>
+    /// Object with info that is required for validation.
+    /// </summary>
     public class PropertyValidatorContext
     {
+        /// <summary>
+        /// Gets the object to validate.
+        /// </summary>
         public object ObjectToValidate { get; private set; }
+
+        /// <summary>
+        /// Gets the property of <see cref="ObjectToValidate"/> to validate.
+        /// </summary>
         public object PropertyToValidate { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="PropertyValidatorContext"/> class.
+        /// </summary>
+        /// <param name="objectToValidate">The object to validate</param>
+        /// <param name="propertyToValidate">The property of <paramref name="objectToValidate"/> to validate</param>
         public PropertyValidatorContext(object objectToValidate, object propertyToValidate)
         {
             objectToValidate.Guard(nameof(objectToValidate));
